270e5e7a271fa5cd45bf32a01634c7481b10b9ee
raszidzie <charliescene512@gmail.com>
2020-06-14 20:29:03 +0200

    refactor endpoint views

--- app/wash/views.py
+++ app/wash/views.py
@@ -5,31 +5,22 @@ from rest_framework.permissions import IsAuthenticated
 from core.models import Tag, Clothe
 from wash import serializers
 
-class TagViewSet(viewsets.GenericViewSet, 
-                 mixins.ListModelMixin,
-                 mixins.CreateModelMixin):
+class BaseWashAttrViewSet(viewsets.GenericViewSet,
+                          mixins.ListModelMixin,
+                          mixins.CreateModelMixin):
     authentication_classes = (TokenAuthentication, )
-    permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAuthenticated, )
+
+    def get_queryset(self):
+        return self.queryset.filter(user=self.request.user).order_by('-name')
+    
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+class TagViewSet(BaseWashAttrViewSet):
     queryset = Tag.objects.all()
     serializer_class = serializers.TagSerializer
 
-    def get_queryset(self):
-        return self.queryset.filter(user=self.request.user).order_by('-name')
-    
-    def perform_create(self, serializer):
-        serializer.save(user=self.request.user)
-
-class ClotheViewSet(viewsets.GenericViewSet,
-                    mixins.ListModelMixin,
-                    mixins.CreateModelMixin):
-
-    authentication_classes = (TokenAuthentication, )
-    permission_classes = (IsAuthenticated, )
+class ClotheViewSet(BaseWashAttrViewSet):
     queryset = Clothe.objects.all()
     serializer_class = serializers.ClotheSerializer
-
-    def get_queryset(self):
-        return self.queryset.filter(user=self.request.user).order_by('-name')
-    
-    def perform_create(self, serializer):
-        serializer.save(user=self.request.user)
